315ad15f88cb10bcfa04fadb8ddee4c7c470cfbe
Miguel <delgdevoleper@gmail.com>
2024-08-24 12:32:02 -0300

    project created

---
+++ src/main/java/com/migueldelgado/state_manager/StateManagerApplication.java
@@ -0,0 +1,13 @@
+package com.migueldelgado.state_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migueldelgado/state_manager/StateManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migueldelgado.state_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
